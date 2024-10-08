2ef51da84d28718f094992c2925bc0a610b2dfb1
victoriamuinde07 <victroiamuinde07@gmail.com>
2024-10-08 20:48:42 +0300

    modified code

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,15 +7,22 @@
 </script>
 
 <header class={`sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ${y > 0 ? 'py-4 bg-slate-950 border-violet-950' : 'py-6 bg-transparent border-transparent'}`}>
-    <h1>
-        <b>Victoria</b>Mwikali
+    <h1 class = "font-medium">
+        <b class="font-bold poppins">Victoria</b>Mwikali
     </h1>
-    
-        <div class="">
+
+        <div class="sm:flex items_center gap-4 hidden">
             {#each tabs as tab, index}
-                <a>
+                <a href={tab.link} class ="duration-200 hover:text-violet-400">
                     <p>{tab.name}</p>
                 </a>
             {/each}
         </div>
+        <a class="blueshadow relative overflow-hidden px-5 group rounded-full bg-white text-slate-950">
+            <div class = "absolute top-0 right-full w-full bg-violet-400 opacity-20 
+            group-hover:translate-x-full z-0 duration-200">
+        
+        </div>
+        <h4 class="relative z-9">Get in touch</h4>
+        </a>
 </header>
